c8c9bf3342b927f3d00e287afc87ad513d4278c6
Moises Marcano <92418515+Hazuru00@users.noreply.github.com>
2025-11-24 14:50:48 -0400

    100% responsive now

--- PORTAFOLIO2/src/components/NavBar.tsx
+++ PORTAFOLIO2/src/components/NavBar.tsx
@@ -1,13 +1,13 @@
 export default function Navbar_() {
     return (
-        <nav className="bg-gray-800 p-4 h-auto pb-2 min-w-xl ">
-            <div className="mx-auto flex justify-between items-center ">
+        <nav className="bg-gray-800 p-4 pb-2 w-full h-auto">
+            <div className="mx-auto flex justify-between items-center w-full flex-wrap">
                 <div className="text-purple-500 font-bold text-xl">My Portfolio</div>
-                <div className="">
-                    <a href="#home" className="text-purple-500 hover:text-white px-3">Home</a>
+                <div className="flex-wrap flex">
+                    <a href="#home" className="text-purple-500 hover:text-white pr-3">Home</a>
                     <a href="#about" className="text-purple-500 hover:text-white px-3">About</a>
                     <a href="#projects" className="text-purple-500 hover:text-white px-3">Projects</a>
-                    <a href="#contact" className="text-purple-500 hover:text-white px-3">Contact</a>
+                    <a href="#contact" className="text-purple-500 hover:text-white px-3 ">Contact</a>
                 </div>
             </div>
         </nav>
---
+++ PORTAFOLIO2/src/components/profile.tsx
@@ -0,0 +1,54 @@
+import { MapPin } from 'lucide-react';
+import { Mail } from 'lucide-react';
+import { FileText } from 'lucide-react';
+import { Github } from 'lucide-react';
+import { Linkedin } from 'lucide-react';
+
+export default function Profile() {
+    return (
+        <div id='Home' className='flex justify-center m-0 items-center min-w-auto mb-3'>
+            <span className='max-w-32  '>
+                <img className='w-full rounded-full' src='https://avatars.githubusercontent.com/u/92418515?v=4' alt='GithubProfileIMG' />
+
+            </span>
+            <div className='flex flex-col gap-2 mt-7 mb-3 ml-7  '>
+                <h1 className=' font-stretch-normal text-4xl w-fit'><strong>Moises A. Marcano (Hazuru)</strong> </h1>
+
+                <h2 className=' text-3xl text-purple-500'>Full Stack Developer | Frontend Developer</h2>
+                <div className='flex-row w-fit flex gap-2 items-center'>
+                    <div><MapPin /></div> <div><h3 className='text-xl'>Caracas, Venezuela</h3></div>
+                </div>
+                <div className='flex flex-wrap w-fit mt-2 gap-2'>
+
+
+                    <div className='flex-wrap flex items-center gap-3 h-max-8'>
+                        <a href="mailto:HazuruCh@proton.me"><button className='hover:cursor-pointer hover:bg-purple-600   hover:outline-2 hover:outline-purple-500 hover:outline-offset-2px flex-wrap flex items-center gap-3 bg-accent rounded-4xl p-0.5 px-2 font-sans text-xl'><Mail /> HazuruCh@proton.me</button></a>
+
+
+                    </div>
+                    <div className='h-max-8 flex flex-wrap gap-1 w-max-auto'>
+
+                        <a target='_blank' href="">
+                            <button className='hover:cursor-pointer hover:outline-2 hover:border-accent flex-wrap flex items-center gap-3 bg-violet-950 border-2 border-purple-700 rounded-4xl p-1 px-2 font-sans text-xl'>
+                                <FileText color={'oklch(62.7% 0.265 303.9)'} />
+                            </button>
+                        </a>
+
+                        <a target='_blank' href="https://github.com/Hazuru00">
+                            <button className='hover:cursor-pointer hover:outline-2 hover:border-accent flex-wrap flex items-center gap-3 bg-violet-950 border-2 border-purple-700 rounded-4xl p-1 px-2 font-sans text-xl'>
+                                <Github color={'oklch(62.7% 0.265 303.9)'} />
+                            </button>
+                        </a>
+
+                        <a target='_blank' href="https://www.linkedin.com/in/moises-marcano-b85363389">
+                            <button className='hover:cursor-pointer hover:outline-2 hover:border-accent flex-wrap flex items-center gap-3 bg-violet-950 border-2 border-purple-700 rounded-4xl p-1 px-2 font-sans text-xl'>
+                                <Linkedin color={'oklch(62.7% 0.265 303.9)'} />
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
