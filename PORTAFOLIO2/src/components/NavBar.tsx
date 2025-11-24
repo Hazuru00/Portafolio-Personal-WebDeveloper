f78735540afea32253730bde3300fb553000b6fb
Moises Marcano <92418515+Hazuru00@users.noreply.github.com>
2025-11-24 15:21:18 -0400

    litoooo

--- PORTAFOLIO2/src/components/NavBar.tsx
+++ PORTAFOLIO2/src/components/NavBar.tsx
@@ -2,15 +2,15 @@ export default function Navbar_() {
     return (
         <nav className="bg-gray-800 p-4 pb-2 w-full h-auto">
             <div className="mx-auto flex justify-between items-center w-full flex-wrap">
-                <div className="text-purple-500 font-bold text-xl">My Portfolio</div>
-                <div className="flex-wrap flex">
-                    <div>
+                <div className="text-purple-500 font-bold text-xl mb-2 w-66 flex justify-center"><span>My Portfolio</span></div>
+                <div className="flex-wrap flex justify-center">
+                    <div className="">
                         <a href="#home" className="text-purple-500 hover:text-white pr-3">Home</a>
                         <a href="#about" className="text-purple-500 hover:text-white px-3">About</a>
 
                     </div>
-                    <div>
-                        <a href="#projects" className="text-purple-500 hover:text-white px-3">Projects</a>
+                    <div className="">
+                        <a href="#projects" className="text-purple-500 hover:text-white pr-3">Projects</a>
                         <a href="#contact" className="text-purple-500 hover:text-white px-3 ">Contact</a>
                     </div>
                 </div>
