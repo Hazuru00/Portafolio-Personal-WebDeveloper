7218a2a6a95c196ba279562f5f180d90c6a33d55
Moises Marcano <92418515+Hazuru00@users.noreply.github.com>
2025-11-24 22:08:23 -0400

    spanish

--- PORTAFOLIO2/src/components/NavBar.tsx
+++ PORTAFOLIO2/src/components/NavBar.tsx
@@ -3,18 +3,18 @@ export default function Navbar_() {
         <nav className="bg-gray-800 p-3 pb-2 w-full h-auto">
             <div className="justify-center m-auto">
                 <div>
-                    <div className="text-purple-500 font-bold text-xl mb-2 flex justify-center "><span>🔰 My Portfolio 🔰</span></div>
+                    <div className="text-purple-500 font-bold text-xl mb-2 flex justify-center "><span>🔰 HazuruDEV 🔰</span></div>
 
                     <div className="mx-auto flex justify-center items-center w-full flex-wrap m-auto">
                         <div className="flex-wrap flex justify-center">
                             <div className="">
                                 <a href="#home" className="text-purple-500 hover:text-white pr-3">Home</a>
-                                <a href="#about" className="text-purple-500 hover:text-white px-3">About</a>
+                                <a href="#about" className="text-purple-500 hover:text-white px-3">Sobre mi</a>
 
                             </div>
                             <div className="">
-                                <a href="#projects" className="text-purple-500 hover:text-white pr-3">Projects</a>
-                                <a href="#contact" className="text-purple-500 hover:text-white px-3 ">Contact</a>
+                                <a href="#projects" className="text-purple-500 hover:text-white pr-3">Proyectos</a>
+                                <a href="#contact" className="text-purple-500 hover:text-white px-3 ">Contacto</a>
                             </div>
                         </div>
                     </div>
